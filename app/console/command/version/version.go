493a2455b3947d63b7ab8027c9214bdc6ee7e968
zhaoyang <y.zhao@avid.ly>
2022-06-23 15:58:18 +0800

    将module改为ginco

--- app/console/command/version/version.go
+++ app/console/command/version/version.go
@@ -1,13 +1,9 @@
 package version
 
 import (
-	"context"
 	"fmt"
 	"ginco/framework/contract"
-	"ginco/framework/elasticsearch"
-	"github.com/olivere/elastic/v7"
 	"github.com/spf13/cobra"
-	"reflect"
 )
 
 type Person struct {
@@ -23,32 +19,6 @@ func Command(a contract.Application) *cobra.Command {
 		Long:  "Get Application version",
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Printf("The Application version is v%s\n", a.Version())
-
-			ctx := context.Background()
-			es := a.GetI("es").(*elasticsearch.Elasticsearch)
-
-			info, code, err := es.Ping("http://192.168.17.129:9200").Do(ctx)
-			if err != nil {
-				panic(err)
-			}
-			fmt.Printf("Elasticsearch returned with code %d and version %s\n", code, info.Version.Number)
-
-			query := elastic.NewMatchAllQuery()
-			resp, err := es.Search().Index("user").Query(query).Do(ctx)
-			if err != nil {
-				panic(err)
-			}
-
-			var person Person
-			var persons []Person
-			for _, item := range resp.Each(reflect.TypeOf(person)) {
-				if t, ok := item.(Person); ok {
-					persons = append(persons, t)
-				}
-			}
-
-			fmt.Printf("persons:\n %+#v\n", persons)
-
 		},
 	}
 }
